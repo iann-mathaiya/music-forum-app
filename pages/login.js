56cee65266626aa0bd8f03f591e7df40a3337e3c
Ian Mathaiya <iannmathaiya@gmail.com>
2021-12-15 23:18:55 +0300

    new commit

--- pages/login.js
+++ pages/login.js
@@ -1,16 +1,10 @@
-import { useState, useEffect } from 'react'
-import { supabase } from '../utils/supabaseClient'
 import Auth from './components/auth'
-import Sidenav from './components/sidenav'
-import Navbar from './components/navbar'
-import Forum from './components/forum'
+
 
 export default function Welcome() {
 
     return (
         <>
-
-        <Navbar />
 
         <Auth />
     
